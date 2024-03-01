5817b3cb907d08da21f4ed9c4e443c665ae2ede9
miku <miku71041@gmail.com>
2024-03-01 21:18:25 +0900

    update

---
+++ samplecode6.py
@@ -0,0 +1,17 @@
+#  リストの要素を繰り返し処理する
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers:
+    print(number)
+
+#  範囲を指定して繰り返し処理する
+for i in range(5):
+    print(i)
+
+
+counter = 0
+
+while counter < 5:
+    print(counter)
+    counter += 1
+
